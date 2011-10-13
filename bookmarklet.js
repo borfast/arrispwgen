56b2d12c38ac598143123dcfe9f905fadc570b97
Raúl Santos <borfast@gmail.com>
2011-10-13 19:51:47 +0100

    Making the github site the generator page.

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+(function(){elem=document.createElement('script');elem.type='text/javascript';elem.src='http://github.com/borfast/arrispwgen/raw/master/arrispwgen.js';document.body.appendChild(elem); alert("Today's Arris modem password of the day is: " + GenArrisPasswords((new Date()).getTime()));})();
