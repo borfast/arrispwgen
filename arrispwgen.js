9a3fdac03eaffa578279de81816762dd3fbad09f
Raúl Pedro Santos <borfast@gmail.com>
2013-09-28 00:48:40 +0100

    Get rid of the old license and the reference to TM602A.

--- arrispwgen.js
+++ arrispwgen.js
@@ -1,35 +1,13 @@
 /*
- * Arris TM602A password of the day generator
+ * Arris password of the day generator. For a full list of the supported
+ * modem models, visit the project homepage.
  * 
- * Author: Raul Pedro Fernandes Santos
- * Project homepage: http://www.borfast.com/projects/arrispwgen
- * 
- * This software is distributed under the Simplified BSD License.
- * 
- * Copyright 2011 Raul Pedro Fernandes Santos. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Author: Raúl Pedro Fernandes Santos
+ * Project homepage: http://www.borfast.com/projects/arris-password-of-the-day-generator/
  * 
  */
 
-// TODO: Add some unit testing, just for fun.
+// TODO: Add some testing, just for fun.
 
 function GenArrisPasswords(startdate, enddate) {
 	"use strict";
